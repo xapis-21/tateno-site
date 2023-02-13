5df6cbc577e238f39ffc409ab053ab850f45c07c
xapis-21 <xapisofficial@gmail.com>
2023-02-13 20:25:47 +0300

    Updating site update

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,12 +3,8 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className="bg-cover bg-no-repeat bg-center">{children}</body>
     </html>
   )
 }
